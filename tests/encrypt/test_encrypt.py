f07d8ea1f91ecc38ef8d65ef50a3d27fdc2c5ac9
D-Wolter <wolter.daniel@gmail.com>
2023-06-02 13:58:08 -0300

    feat: req2 criado o teste test_encrypt.py para função challenge_encrypt_message.py

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("", 5) == ""
+    
+    assert encrypt_message("wolter", 2) == ("retl_ow")
+
+    assert encrypt_message("wolter", 3) == ("low_ret")
+
+    assert encrypt_message("wolter", -10) == ("retlow")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 7)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("wolter", "tr")
+
+
