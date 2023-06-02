4b278df774ad14dfc255236c1ff4e94692876f96
D-Wolter <wolter.daniel@gmail.com>
2023-06-02 15:59:55 -0300

    feat: Req3 criada função do arquivo challenges/challenge_palindromes_recursive.py que irá determinar se uma palavra é um palíndromo e retorna True ou False

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # inverse = word[::-1]
+    if len(word) < 1:
+        return False
+    elif low_index >= high_index:
+        return True
+    elif not word[low_index] == word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# word = "REVIVER"
+# is_palindrome_recursive(word, 0, len(word) - 1)
