adbb1ac0dbd1c4f0065c36b3441cf13c0493546f
D-Wolter <wolter.daniel@gmail.com>
2023-06-02 14:02:53 -0300

    feat req2 fix linter

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,7 +4,7 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
     assert encrypt_message("", 5) == ""
-    
+
     assert encrypt_message("wolter", 2) == ("retl_ow")
 
     assert encrypt_message("wolter", 3) == ("low_ret")
@@ -16,5 +16,3 @@ def test_encrypt_message():
 
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("wolter", "tr")
-
-
