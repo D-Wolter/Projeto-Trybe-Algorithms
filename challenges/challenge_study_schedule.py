987b28bd4e6bd8391190316319c1625e8db2a775
D-Wolter <wolter.daniel@gmail.com>
2023-06-02 12:53:38 -0300

    feat: Req 1: criado função study_schedule, que recebe uma lista de tuplas com horarios de entrada e saida dos alunos e um numero inteiro representando um horario especifico, a função retorna a contagem de alunos online nesse horario passado.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if isinstance(target_time, int):
+        only_student_count = 0
+        for login, logout in permanence_period:
+            if not isinstance(login, int) or not isinstance(logout, int):
+                return None
+            if target_time >= login and target_time <= logout:
+                only_student_count += 1
+        return only_student_count
+    else:
+        return None
+
+
+# permanence_period = [(2, 3), (5, 7), (1, 2), (6, 7), (5, 7), (5, 7)]
+
+# print(study_schedule(permanence_period, 6))
